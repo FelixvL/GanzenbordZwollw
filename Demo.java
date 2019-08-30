d8a072fe76fbf00aeb7d8eb59c1aac66b24533fb
FelixvL <digitaleservice@gmail.com>
2019-08-30 12:45:49 +0200

    toevoeging met dobbelsteen

--- Demo.java
+++ Demo.java
@@ -1,8 +1,33 @@
-class Demo{
-    public static void main (String[] args){
+import java.util.Random;
 
-    	
-    }
+public class Demo {
+	public static void main(String[] args){
+		
+		
+		Dobbelsteen dobbelsteen1 = new Dobbelsteen();
+		dobbelsteen1.werpen();
+		
+		Gans gans1 = new Gans();
+		gans1.positie(dobbelsteen1);
+		
+	}
+}
+
+class Dobbelsteen{
+	int uitkomst;
+	void werpen() {
+		Random willekeur = new Random ();
+		int uitkomst = willekeur.nextInt(6);
+		System.out.println(uitkomst + 1);
+	}
 }
 
 
+class Gans{
+	int positie;
+	void positie(Dobbelsteen dobbelsteen) {
+		int positie = 1;
+//		int positie2 = positie + ;
+		System.out.println("Je staat op vakje " + positie);
+	}
+}
