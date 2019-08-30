7cf280caa80e5e2390c29a746104d3ee02a327bb
FelixvL <digitaleservice@gmail.com>
2019-08-30 14:37:43 +0200

    gaan

--- Demo.java
+++ Demo.java
@@ -1,17 +1,45 @@
+    
 import java.util.Random;
+import java.util.Scanner;
 
-public class Demo {
-	public static void main(String[] args){			
-		Dobbelsteen dobbelsteen1 = new Dobbelsteen();
-		dobbelsteen1.werpen();
-
-		Gans gans1 = new Gans();
+public class Demo {	
+	public static void main(String[] args){		
+		Scanner scanner = new Scanner(System.in);
+		Ganzenbord ganzenbord = new Ganzenbord();
 		
-		for(int x = 0; x < 100000 ; x++) {
-			gans1.lopen(dobbelsteen1);
-			dobbelsteen1.werpen();			
-		}
+		ganzenbord.zetten(ganzenbord.ganzen[0]);
+		ganzenbord.zetten(ganzenbord.ganzen[1]);
+		ganzenbord.zetten(ganzenbord.ganzen[2]);
+	}
 
+
+}
+
+
+class Ganzenbord{
+	Gans[] ganzen = new Gans[3];
+	Dobbelsteen dobbelsteen1 = new Dobbelsteen();
+	
+	Ganzenbord() {
+		ganzen[0] = new Gans("Rood");
+		ganzen[1] = new Gans("Geel");
+		ganzen[2] = new Gans("Blauw");
+		
+	}
+	void zetten (Gans gans1) {
+		Scanner scanner = new Scanner(System.in);
+		for(int x = 0; x < 10 ; x++) {
+			gans1.lopen(dobbelsteen1);
+			dobbelsteen1.werpen();	
+			System.out.println("Nog een keer gooien toets G");
+			System.out.println("Stop met gooien toets Q");
+			String invoer = scanner.next();
+			if (invoer.equals("Q")) {
+				break;
+			} else if(invoer.equals("G")) {
+				continue;
+			}		
+		}
 	}
 }
 
@@ -19,15 +47,21 @@ class Dobbelsteen{
 	int uitkomst;
 	void werpen() {
 		Random willekeur = new Random ();
+		
 		uitkomst = willekeur.nextInt(6) + 1;
 		System.out.println("De dobbelsteen werpt: "+ uitkomst);
 	}
+
 }
 
 class Gans{
 	int positie;
+	String kleur;
+	Gans(String kleur){
+		this.kleur = kleur;
+	}
 	void lopen(Dobbelsteen dobbelsteen) {
 		positie = positie + dobbelsteen.uitkomst;
-		System.out.println("Je staat op vakje: " + positie);
+		System.out.println(kleur+"Je staat op vakje: " + positie);
 	}
 }
