263714e9e4f5b37179653ef19a5df103ba9772d8
FelixvL <digitaleservice@gmail.com>
2019-08-30 13:10:37 +0200

    lopen in gans

--- Demo.java
+++ Demo.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 
 public class Demo {
-	public static void main(String[] args){
-		
-		
+	public static void main(String[] args){			
 		Dobbelsteen dobbelsteen1 = new Dobbelsteen();
 		dobbelsteen1.werpen();
-		
+
 		Gans gans1 = new Gans();
-		gans1.positie(dobbelsteen1);
 		
+		for(int x = 0; x < 100000 ; x++) {
+			gans1.lopen(dobbelsteen1);
+			dobbelsteen1.werpen();			
+		}
+
 	}
 }
 
@@ -17,17 +19,15 @@ class Dobbelsteen{
 	int uitkomst;
 	void werpen() {
 		Random willekeur = new Random ();
-		int uitkomst = willekeur.nextInt(6);
-		System.out.println(uitkomst + 1);
+		uitkomst = willekeur.nextInt(6) + 1;
+		System.out.println("De dobbelsteen werpt: "+ uitkomst);
 	}
 }
 
-
 class Gans{
 	int positie;
-	void positie(Dobbelsteen dobbelsteen) {
-		int positie = 1;
-//		int positie2 = positie + ;
-		System.out.println("Je staat op vakje " + positie);
+	void lopen(Dobbelsteen dobbelsteen) {
+		positie = positie + dobbelsteen.uitkomst;
+		System.out.println("Je staat op vakje: " + positie);
 	}
 }
